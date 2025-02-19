f505ae3d5a853ddd6606703742f9beed0cda85a6
funwarioisii <kazuyukihashimoto2006@gmail.com>
2025-02-19 23:55:24 +0900

    Fix ToolsTab Textarea value for clearing when switch tool

--- client/src/components/ToolsTab.tsx
+++ client/src/components/ToolsTab.tsx
@@ -181,6 +181,7 @@ const ToolsTab = ({
                           id={key}
                           name={key}
                           placeholder={prop.description}
+                          value={(params[key] as string) ?? ""}
                           onChange={(e) =>
                             setParams({
                               ...params,
