cdb9180629542dc87fe2b13e0e274277493b3768
Marc Abramowitz <abramowi@adobe.com>
2025-06-07 08:11:23 -0700

    Generate report with https://github.com/daun/playwright-report-summary

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -14,7 +14,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html", { outputFolder: "playwright-report" }], ["line"]],
+  reporter: [
+    ["html", { outputFolder: "playwright-report" }],
+    ["json", { outputFile: "results.json" }],
+    ["line"],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
