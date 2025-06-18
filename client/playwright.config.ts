0f1cc199268d5c8e45dcbdac898948d788c857db
Marc Abramowitz <abramowi@adobe.com>
2025-06-17 20:26:22 -0700

    prettier fix playwright.config.ts

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -6,9 +6,9 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
-    cwd: '..',
-    command: 'npm run dev',
-    url: 'http://localhost:6274',
+    cwd: "..",
+    command: "npm run dev",
+    url: "http://localhost:6274",
     reuseExistingServer: !process.env.CI,
   },
 
