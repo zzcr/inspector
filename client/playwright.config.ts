db9ca3762a9c99501d45f72c4724ad2089e2fa82
Marc Abramowitz <abramowi@adobe.com>
2025-06-19 00:00:32 -0700

    No playwright-report or results.json for non-CI

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -22,11 +22,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
+  reporter: process.env.CI ? [
     ["html", { outputFolder: "playwright-report" }],
     ["json", { outputFile: "results.json" }],
     ["line"],
-  ],
+  ] : [["line"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
